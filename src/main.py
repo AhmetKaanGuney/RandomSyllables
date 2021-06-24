de673bed7a3daa127cb450ced4046e1cd16cebe2
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2021-06-25 01:28:28 +0300

    Remade GUI.
    Added icon
    Added logo image
    Remade the colors

--- src/main.py
+++ src/main.py
@@ -1,25 +1,39 @@
 from gui import *
 from generators import run_generators
 
-while True:
-    event, values = window.read()
 
-    if event is None:
-        break
+def main():
+    while True:
+        event, values = window.read()
 
-    if event == "-GENERATE-":
-        selected_number = None
-        radio_buttons = {1: window["-RADIO1-"], 2: window["-RADIO2-"], 3: window["-RADIO3-"]}
+        if event is None:
+            break
 
-        # Detect radio button
-        for button in radio_buttons:
-            button_value = radio_buttons[button].get()
-            if button_value is True:
-                selected_number = button
+        # -------------------------------------------------- #
+        #      When GENERATE button is pressed generate      #
+        #      a word for 'x' amount of syllables            #
+        #      'x' = whichever radio button is selected      #
+        # -------------------------------------------------- #
+        if event == "-GENERATE-":
+            selected_number = None
+            radio_buttons = {1: window["-RADIO1-"], 2: window["-RADIO2-"], 3: window["-RADIO3-"]}
 
-        # Return Output from generator
-        output = run_generators(selected_number)
-        window["-RESULT-"].update(output)
-        window.refresh()
+            # Detect radio button
+            for button in radio_buttons:
+                button_value = radio_buttons[button].get()
+                if button_value is True:
+                    selected_number = button
 
-window.close()
+            # Return Output from generator
+            output = run_generators(selected_number)
+            window["-RESULT-"].update(output)
+            window.refresh()
+
+    window.close()
+
+
+# ---------------------- #
+#      ENTRY POINT       #
+# ---------------------- #
+if __name__ == "__main__":
+    main()
