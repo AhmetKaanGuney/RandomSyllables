5f3f052ab73c61da85684bcd53f1072e571a5fff
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2021-06-24 11:26:35 +0300

    main.py got cleaned. Has better readability now.

--- src/main.py
+++ src/main.py
@@ -1,56 +1,74 @@
-# todo define letters
-# todo define syllable , algorithm for v=vowel c=consonant so: "vc", "vcv", "vcc", "cv", "cvc"
-# forbidden syllable combos : 'bk' , 'bf' , 'tk' , 'mn' , 'pc' , 'bz'
-# todo random syllable generation
-# todo random word generation from syllables (max 4 syllables)
-# todo max 3 words printed
-# todo input for; reshuffle syllables , generate new word
 import random
 import os
-# ### LETTERS AND SYLLABLE RULES ###
+# TODO allow user to craft their own rules
+# -------------------------------- #
+#    LETTERS AND SYLLABLE RULES    #
+# -------------------------------- #
+# alphabet = "abcdefghijklmnopqrstuvwxyz"  # 26 letters
+consonants = "bcdfghklmnprstvyz"  # 17 letters,  carefully chosen
 vowels = "aeiou"  # 5 letters
-consonants = "bcdfghjklmnpqrstvwxyz"  # 20 letters
-alphabet = "abcdefghijklmnopqrstuvwxyz"  # 26 letters
+
+# These are specifically chosen rules to have better results
 rule0 = "vc"
 rule1 = "vcv"
-rule2 = "vcc"
-rule3 = "cv"
-rule4 = "cvc"
-syllable_rules = [rule0, rule1, rule2, rule3, rule4]  # 5 items (0-4)
-# ### LETTER DICTIONARY
-dict_syll = {}
-# ### RANDOM VALUES
+rule2 = "cv"
+rule3 = "cvc"
+syllable_rules = [rule0, rule1, rule3]  # 5 items (0-4)
+
+# ------------------------ #
+#      RANDOM VALUES       #
+# ------------------------ #
 ran_cons = random.randrange(0, 20)
 ran_vow = random.randrange(0, 4)
 
 
-# ### Syllable Generator
-def random_syll(syll_rules):
-    x = 0
-    output = []
-    random_syll_rule = random.choice(syll_rules)
+# ------------------------ #
+#    SYLLABLE GENERATOR    #
+# ------------------------ #
+def generate_random_syllable():
+    syllable = []
+    random_syll_rule = random.choice(syllable_rules)
     syll_range = range(len(random_syll_rule))
     # print("random rule is ", random_syllable_rule)
     for letter in syll_range:
         if random_syll_rule[letter] == "v":
-            output.append(random.choice(vowels))
+            syllable.append(random.choice(vowels))
         else:
-            output.append(random.choice(consonants))
-        x += 1
-    return output
+            syllable.append(random.choice(consonants))
+
+    return syllable
 
 
-# ### USER INPUT
-user_input = int(input("How many syllables do you want? : "))
-# ### Word Generator
+# ------------------------------------  #
+#      ASK THE AMOUNT OF SYLLABLES      #
+# ------------------------------------  #
+user_input = -1  # this is just a placeholder
+while user_input <= 0:
+    user_input = int(input("How many syllables do you want? : "))
+
+
+# ------------------------ #
+#      WORD GENERATOR      #
+# ------------------------ #
+def generate_word():
+    word_list = []
+    for n in range(0, user_input):
+        syllable = generate_random_syllable()
+        word_list.append("".join(syllable))
+    word = "".join(word_list)
+    capitalized_word = word.capitalize()
+    return capitalized_word
+
+
+# ------------------------ #
+#      OUTPUT RESULTS      #
+# ------------------------ #
 while user_input != 0:
     for i in range(0, 3):
-        word_list = []
-        for n in range(0, user_input):
-            syll_i = random_syll(syllable_rules)
-            word_list.append("".join(syll_i))
-        word = "".join(word_list)
-        print(word.capitalize())
+        output = generate_word()
+        print(output)
+
+    # Go to the next iteration
     input("---Next---")
     os.system("cls")
 else:
