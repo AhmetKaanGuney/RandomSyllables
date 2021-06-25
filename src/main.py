61eb4434f8207da8dd9f2460c4af17d928a6663c
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2021-06-25 17:52:53 +0300

    Done some minor adjustments.

--- src/main.py
+++ src/main.py
@@ -16,15 +16,15 @@ def main():
         # -------------------------------------------------- #
         if event == "-GENERATE-":
             selected_number = None
-            radio_buttons = {1: window["-RADIO1-"], 2: window["-RADIO2-"], 3: window["-RADIO3-"]}
+            r_buttons = {1: window["-RADIO1-"], 2: window["-RADIO2-"], 3: window["-RADIO3-"]}
 
             # Detect radio button
-            for button in radio_buttons:
-                button_value = radio_buttons[button].get()
+            for button in r_buttons:
+                button_value = r_buttons[button].get()
                 if button_value is True:
                     selected_number = button
 
-            # Return Output from generator
+            # Print output to TextBox on the GUI
             output = run_generators(selected_number)
             window["-RESULT-"].update(output)
             window.refresh()
